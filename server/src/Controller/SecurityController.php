0b4be3ef5e858eb8a76efe1539a876c1c457aaeb
julen <julendemi02@gmail.com>
2023-02-14 17:44:10 +0100

    SecurityController y formulario log in creado

---
+++ server/src/Controller/SecurityController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\UserMgr\User;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Controller\UserPasswordEncoderInterface;
+
+class SecurityController extends AbstractController
+{
+    #[Route('/register', name: 'register', methods: 'post')]   
+     
+    public function register(ManagerRegistry $doctrine, Request $request, UserPasswordEncoderInterface $encoder)
+    {
+        $em = $this->$doctrine->getManager();
+        // IMP! To get JSON format from POST method
+        $data = $request->getContent();
+        $content = json_decode($data);
+
+        $username = $content->username;
+        $password = $content->password;
+
+        $user = new User($username);
+        $user->setPassword($encoder->encodePassword($user, $password));
+
+        $em->persist($user);
+        $em->flush();
+
+        return new Response(sprintf('User %s successfully created', $user->getUsername()));
+    }
+    
+}
